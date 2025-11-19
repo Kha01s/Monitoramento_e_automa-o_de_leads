b25ad602191a8d1d4512606e391b45c58268461f
Khaos.deb <dimitriezarel@gmail.com>
2025-11-19 14:06:13 -0300

    update

---
+++ projeto_inicial/back/automacao.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+
+print('Iniciando a coleta...')
+dados=[]
+for i in range(40):
+    data = datetime.now() - timedelta(days=i)
+    leads = random.randint(6, 80)
+    dados.append({"Data": data.strftime("%d-%m-%Y"), "Leads": leads})
+
+df = pd.DataFrame(dados)
+
+df.to_csv("dados_vendas.csv", index=False)
+print("Automação finalizada, arquivo gerado!")
---
+++ projeto_inicial/dashboard/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Dashboard de VENDAS", layout="wide")
+st.title("Dashboard de Performance")
+
+try:
+    df = pd.read_csv("dados_vendas.csv")
+
+    total_leads = df["Leads"].sum()
+    media_leads = df["Leads"].mean()
+
+    col1, col2 = st.columns(2)
+    col1.metric("Total de Leads", total_leads)
+    col2.metric("Média Diária", f"{media_leads:.1f}")
+
+    st.subheader("Evolução no Tempo")
+    st.bar_chart(df, x="Data", y="Leads")
+
+except FileNotFoundError:
+    st.error('Rode o script de automação primeiro!')
